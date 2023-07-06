d75bb149f05633514433cf5789da1db313be2012
hp <hp@DESKTOP-GRQT257>
2023-07-06 11:44:12 +0530

    [pardip chaudhari] this is prdips message to the rupesh and ganesh

---
+++ src/main/java/test/Ganesh.java
@@ -0,0 +1,11 @@
+package test;
+
+public class Ganesh {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("hello ganesh how are you");
+
+	}
+
+}
---
+++ src/main/java/test/Rupesh.java
@@ -0,0 +1,12 @@
+package test;
+
+public class Rupesh {
+
+	public static void main(String[] args)
+	{
+
+		System.out.println("hello rupesh how are you.....");
+		
+	}
+
+}
